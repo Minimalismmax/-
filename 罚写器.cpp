af0f9ec503918532b0e407a9550ccffad62173d1
Minimalismmax <epyc876543@qq.com>
2026-09-14 21:39:47 +0800

    Add files via upload

---
+++ 罚写器.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	int c=0;
+	string n;
+	cout<<"ÇëÊäÈë·£Ð´ÄÚÈÝ"; 
+	cin>>n;
+	cout<<"ÇëÊäÈë·£Ð´±éÊý";
+	cin>>c;
+	for(int i=1;i<=c;i++){
+		cout<<n<<endl;
+	}
+}
